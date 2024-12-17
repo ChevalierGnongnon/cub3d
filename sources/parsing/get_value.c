0772b5bc3604f0b5c8b6ae42af8735734afc01e9
chhoflac <chhoflac@student.42.fr>
2024-12-17 16:37:37 +0100

    norme

--- sources/parsing/get_value.c
+++ sources/parsing/get_value.c
@@ -6,7 +6,7 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 12:09:30 by chhoflac          #+#    #+#             */
-/*   Updated: 2024/12/17 15:36:45 by chhoflac         ###   ########.fr       */
+/*   Updated: 2024/12/17 16:36:52 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,31 @@ static int	check_line(char *line, int key)
 	return (1);
 }
 
-const char	*get_value(char *line, char *key, size_t start, int *count)
+static const char	*get_wall_texture(char *line, int start, int end)
 {
 	const char	*path;
+	
+	while (line[end] && !is_whitespace(line[end]))
+			end++;
+	path = ft_substr(line, start, end - start);
+	if (!path)
+		return (NULL);
+	return (path);
+}
+
+static const char	*get_rgb(char *line, int start, int end)
+{
+	const char	*rgb;
+	
+	while (line[end])
+		end++;
+	rgb = ft_substr(line, start, end - start);
+	if (!rgb)
+		return (NULL);
+	return (rgb);	
+}
+const char	*get_value(char *line, char *key, size_t start, int *count)
+{
 	int			end;
 
 	(*count)++;
@@ -45,22 +67,8 @@ const char	*get_value(char *line, char *key, size_t start, int *count)
 	end = start;
 	if ((ft_strcmp(key, "C") || ft_strcmp(key, "F"))
 		&& check_line(line, start))
-	{
-		while (line[end] && !is_whitespace(line[end]))
-			end++;
-		path = ft_substr(line, start, end - start);
-		if (!path)
-			return (NULL);
-		return (path);
-	}
+		return (get_wall_texture(line, start, end));
 	else
-	{
-		while (line[end])
-			end++;
-		path = ft_substr(line, start, end - start);
-		if (!path)
-			return (NULL);
-		return (path);
-	}
+		return (get_rgb(line, start, end));
 	return (NULL);
 }
