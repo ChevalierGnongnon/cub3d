dc9ccaf73a42b96565dc7c8b533e61f96309b8ef
chhoflac <chhoflac@student.42.fr>
2024-12-17 17:21:19 +0100

    fix multiple elements in one line

--- sources/parsing/get_value.c
+++ sources/parsing/get_value.c
@@ -6,7 +6,7 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 12:09:30 by chhoflac          #+#    #+#             */
-/*   Updated: 2024/12/17 16:36:52 by chhoflac         ###   ########.fr       */
+/*   Updated: 2024/12/17 17:20:16 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,7 @@ static const char	*get_wall_texture(char *line, int start, int end)
 	path = ft_substr(line, start, end - start);
 	if (!path)
 		return (NULL);
+	printf("%s\n", path);
 	return (path);
 }
 
@@ -51,8 +52,10 @@ static const char	*get_rgb(char *line, int start, int end)
 	rgb = ft_substr(line, start, end - start);
 	if (!rgb)
 		return (NULL);
+	
 	return (rgb);	
 }
+
 const char	*get_value(char *line, char *key, size_t start, int *count)
 {
 	int			end;
@@ -65,8 +68,9 @@ const char	*get_value(char *line, char *key, size_t start, int *count)
 	if (start == ft_strlen(line))
 		return (NULL);
 	end = start;
-	if ((ft_strcmp(key, "C") || ft_strcmp(key, "F"))
-		&& check_line(line, start))
+	if (!check_line(line, start))
+		return (NULL);
+	if (ft_strcmp(key, "C") || ft_strcmp(key, "F"))
 		return (get_wall_texture(line, start, end));
 	else
 		return (get_rgb(line, start, end));
