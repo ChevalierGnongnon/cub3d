b7ac96cad784eb347e267e1a352cf4526d12e194
chhoflac <chhoflac@student.42.fr>
2025-01-25 15:44:21 +0100

    some patchings

--- sources/parsing/rgb_parse.c
+++ sources/parsing/rgb_parse.c
@@ -6,7 +6,7 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 12:37:38 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/01/23 16:37:47 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/01/25 15:35:53 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,31 @@ static int	rgb_checker(const char *srgb)
 	return (1);
 }
 
+int value_checker(const char **trgb)
+{
+	int	i;
+	int	j;
+	
+	i = 0;
+	j = 0;
+	while (trgb[i])
+	{
+		j = 0;
+		while (trgb[i][j] && is_whitespace(trgb[i][j]))
+			j++;
+		while (trgb[i][j] && ft_isdigit(trgb[i][j]))
+			j++;
+		while (trgb[i][j])
+		{
+			if (!is_whitespace(trgb[i][j]))
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
 static t_rgb *rgb_get(const char *srgb)
 {
 	const char	**trgb;
@@ -38,14 +63,13 @@ static t_rgb *rgb_get(const char *srgb)
 	trgb = ft_split(srgb, ',');
 	if (!trgb)
 		return (NULL);
+	if (!value_checker(trgb))
+		return (free_two_dimentional(trgb));
 	if (map_size(trgb) > 4)
 		return (NULL);
 	rgb = malloc(sizeof(t_rgb));
 	if (!rgb)
-	{
-		free_two_dimentional(trgb);
-		return (NULL);
-	}
+		return (free_two_dimentional(trgb));
 	rgb->red = ft_atou(trgb[0]);
 	rgb->green = ft_atou(trgb[1]);
 	rgb->blue = ft_atou(trgb[2]);
@@ -64,6 +88,8 @@ int rgb_check(t_rgb *rgb)
 	if (rgb->green < 0 || rgb->green > 255)
 		return (0);
 	if (rgb->blue < 0 || rgb->blue > 255)
+		return (0);
+	if (rgb->alpha < 0 || rgb->alpha > 255)
 		return (0);
 	return (1);
 }
@@ -88,7 +114,8 @@ unsigned int rgb_convert(const char *srgb)
 		value = 0;
 		value += rgb->red << 24 | rgb->green << 16 | rgb->blue << 8 | rgb->alpha;
 		free(rgb);
+		printf("%x\n", value);
 		return (value);
 	}
-	return (0);
+	return (err_int("rgb entry is missing or invalid.\n", -1));
 }
