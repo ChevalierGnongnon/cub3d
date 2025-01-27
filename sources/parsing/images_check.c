1f8574e2c0a8373138489ff3a5c18217a7b1516b
chhoflac <chhoflac@student.42.fr>
2025-01-27 14:14:40 +0100

    patch images size

--- sources/parsing/images_check.c
+++ sources/parsing/images_check.c
@@ -6,7 +6,7 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 10:58:15 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/01/27 13:13:27 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/01/27 14:14:05 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 static int check_graphics(t_graphics *graphics)
 {
 	if (graphics->wall_east == NULL
-		|| (graphics->wall_east->width > 64 || graphics->wall_east->height > 64))
+		|| (graphics->wall_east->width !=  225 && graphics->wall_east->height !=  225))
 		return (err_int("East wall is missing or in wrong shape", 0));
 	if (graphics->wall_west == NULL
-		|| (graphics->wall_west->width > 64 || graphics->wall_west->height > 64))
+		|| (graphics->wall_west->width !=  225 && graphics->wall_west->height !=  225))
 		return (err_int("West wall is missing or in wrong shape", 0));
 	if (graphics->wall_north == NULL
-		|| (graphics->wall_north->width > 64 || graphics->wall_north->height > 64))
+		|| (graphics->wall_north->width !=  225 && graphics->wall_north->height !=  225))
 		return (err_int("North wall is missing or in wrong shape", 0));
 	if (graphics->wall_south == NULL
-		|| (graphics->wall_south->width > 64 || graphics->wall_south->height > 64))
+		|| (graphics->wall_south->width !=  225 && graphics->wall_south->height !=  225))
 		return (err_int("South wall is missing or in wrong shape", 0));
 	return (1);
 }
@@ -41,6 +41,7 @@ static mlx_image_t	*get_image(mlx_t *mlx, const char *path)
 	if (!image)
 		return (NULL);
 	mlx_delete_texture(texture);
+	printf("%u\n", image->width);
 	return (image);
 }
 
