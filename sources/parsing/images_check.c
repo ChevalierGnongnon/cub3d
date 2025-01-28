d05ac4563c602c6a1491f8daf8c4bea718c17a2c
chhoflac <chhoflac@student.42.fr>
2025-01-28 11:36:01 +0100

    patch images size 128x128, tests ok

--- sources/parsing/images_check.c
+++ sources/parsing/images_check.c
@@ -6,7 +6,7 @@
 /*   By: chhoflac <chhoflac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/30 10:58:15 by chhoflac          #+#    #+#             */
-/*   Updated: 2025/01/27 15:26:41 by chhoflac         ###   ########.fr       */
+/*   Updated: 2025/01/28 11:31:56 by chhoflac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 static int check_graphics(t_graphics *graphics)
 {
 	if (graphics->wall_east == NULL
-		|| (graphics->wall_east->width !=  225 && graphics->wall_east->height !=  225))
+		|| (graphics->wall_east->width != 128 && graphics->wall_east->height != 128))
 		return (err_int("East wall is missing or in wrong shape", 0));
 	if (graphics->wall_west == NULL
-		|| (graphics->wall_west->width !=  225 && graphics->wall_west->height !=  225))
+		|| (graphics->wall_west->width != 128 && graphics->wall_west->height != 128))
 		return (err_int("West wall is missing or in wrong shape", 0));
 	if (graphics->wall_north == NULL
-		|| (graphics->wall_north->width !=  225 && graphics->wall_north->height !=  225))
+		|| (graphics->wall_north->width != 128 && graphics->wall_north->height != 128))
 		return (err_int("North wall is missing or in wrong shape", 0));
 	if (graphics->wall_south == NULL
-		|| (graphics->wall_south->width !=  225 && graphics->wall_south->height !=  225))
+		|| (graphics->wall_south->width != 128 && graphics->wall_south->height != 128))
 		return (err_int("South wall is missing or in wrong shape", 0));
 	return (1);
 }
@@ -53,6 +53,7 @@ static mlx_image_t	*get_image(mlx_t *mlx, const char *path)
 	image = mlx_texture_to_image(mlx, texture);
 	if (!image)
 		return (NULL);
+	printf("%d %d\n", image->width, image->height);
 	mlx_delete_texture(texture);
 	return (image);
 }
